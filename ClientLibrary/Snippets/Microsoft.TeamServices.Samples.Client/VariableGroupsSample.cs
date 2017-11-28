3c2d541cc410be36942123daee339c9363537e30
pranpati <pranpati@microsoft.com>
2017-11-28 11:12:28 +0530

    added VG samples

---
+++ ClientLibrary/Snippets/Microsoft.TeamServices.Samples.Client/VariableGroupsSample.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.TeamServices.Samples.Client.VariableGroup
+{
+    class VariableGroupsSample : ClientSample
+    {
+        [ClientSampleMethod]
+        public VariableGroup Add()
+        {
+
+        }
+    }
+}
